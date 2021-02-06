2a3ddec4c1f0d6305845887946456d01b7bb7210
jjarrett21 <james.jarrett@okstate.edu>
2021-02-05 21:45:00 -0600

    names?

--- scrapey.py
+++ scrapey.py
@@ -6,16 +6,24 @@ import re
 
 html_page = requests.get('https://www.1999.co.jp/eng/image/10522238')
 soup = BeautifulSoup(html_page.content, 'html.parser')
+
 images = soup.find_all('img', {'src':re.compile('jpg')})
 image_link_list = []
 
 for image in images:
+  
   fullImg = 'https://www.1999.co.jp' + image['src']
   image_link_list.append(fullImg)
+  
 
 for i in range(len(image_link_list)):
+  name = image['src']
+  print(name)
   try:
-    urllib.request.urlretrieve(image_link_list[i])
+    urllib.request.urlretrieve(image_link_list[i], name)
+    print(html_page.status_code)
+   
   except:
-    print('stuff not found')
+    # print('stuff not found')
+    print(html_page.status_code)
 
