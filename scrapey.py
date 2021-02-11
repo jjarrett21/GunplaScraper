1c64399c7869cc587e6e696d208d7982fb55a1ae
jjarrett21 <james.jarrett@okstate.edu>
2021-02-10 21:47:14 -0600

    updates, gets things to firebase, readme changes, updates to req

--- scrapey.py
+++ scrapey.py
@@ -3,16 +3,18 @@ from bs4 import BeautifulSoup
 import firebase_admin
 from firebase_admin import credentials, firestore, storage
 from urllib.request import urlopen, Request, urlretrieve
+from google.cloud import client
 import urllib
 import re
 import os 
 
 cred = credentials.ApplicationDefault()
 firebase_admin.initialize_app(cred, {
-  'projectId' : 'g-reader-5aafb'
+  'storageBucket' : 'g-reader-5aafb.appspot.com'
 
 })
 
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] ='/Users/jjarrett/gcloud_key_pair.json'
 
 
 link = input("Please enter link \n")
@@ -24,10 +26,6 @@ image_link_list = []
 
 db = firestore.client()
 bucket = storage.bucket()
-blob = bucket.blob('gunpla')
-
-
-
 
 for img in image_div.find_all('img', { 'src':re.compile('jpg')}):
   fullImg = 'https://www.1999.co.jp' + img['src']
@@ -38,9 +36,10 @@ for i in range(len(image_link_list)):
   path = '/Users/jjarrett/webscraper/img'
   name = str(i) + '.jpg'
   location = os.path.join(path, name)
+  blob = bucket.blob(name)
   try:
     urllib.request.urlretrieve(image_link_list[i], location)
-    blob.upload_from_filename(location)
+    blob.upload_from_filename(name)
     print('grabbing images...')
 
   except:
